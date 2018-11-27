e8454ef30812c7763417a8c1a700143c9e79ed1b
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-27 11:11:03 +0100

    Hallo Welt

--- HalloWelt/Program.cs
+++ HalloWelt/Program.cs
@@ -8,8 +8,26 @@ namespace HalloWelt
 {
     class Program
     {
+        // Einstiegspunkt in das Konsolenprogramm
         static void Main(string[] args)
         {
+            #region Hallo Welt
+            //Console.WriteLine("Demo 12345");
+
+            //// Snippets: cw + TAB + TAB
+            //// STRG + C / V / X gilt für die gesamte Zeile, wenn nichts ausgewählt ist
+
+            //// STRG + K + C   --- Auskommentieren
+            //// STRG + K + U   --- Kommentar entfernen
+            //Console.WriteLine("Hallo Welt");
+            //Console.WriteLine("Hallo Welt");
+            //Console.WriteLine("Hallo Welt");
+            //Console.WriteLine("Hallo Welt");
+            #endregion
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
