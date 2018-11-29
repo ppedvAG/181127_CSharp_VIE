6c1d324ca062f6079cf22ef82f3215bf7d775f99
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-29 09:19:54 +0100

    Init Schnittstellen

---
+++ Schnittstellen/Händler.cs
@@ -0,0 +1,9 @@
+﻿namespace Schnittstellen
+{
+    class Händler
+    {
+        public Obst Produkt { get; set; }
+        public int Anzahl { get; set; }
+    }
+
+}
---
+++ Schnittstellen/Obst.cs
@@ -0,0 +1,11 @@
+﻿namespace Schnittstellen
+{
+    class Obst
+    {
+        public string Name { get; set; }
+        public string Herkunft { get; set; }
+        public decimal Preis { get; set; }
+        public int Kcal { get; set; }
+    }
+
+}
