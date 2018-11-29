754c9ee62ff79b36c15f4b6d7b7619e8ea18dfca
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-29 12:06:44 +0100

    Event

---
+++ DelegatenUndEreignisse/Button.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatenUndEreignisse
+{
+    class Button
+    {
+        // Variante "Lang"
+        //private Action KlickLogik;
+        //public event Action KlickEvent
+        //{
+        //    add
+        //    {
+        //        KlickLogik += value;
+        //    }
+        //    remove
+        //    {
+        //        KlickLogik -= value;
+        //    }
+        //}
+
+        // "AutoProperty" für Events
+        public event Action KlickEvent;
+
+        public void Klick()
+        {
+            if (KlickEvent != null)
+                KlickEvent.Invoke();
+        }
+    }
+}
--- DelegatenUndEreignisse/Program.cs
+++ DelegatenUndEreignisse/Program.cs
@@ -12,37 +12,67 @@ namespace DelegatenUndEreignisse
         public delegate void MeinDelegat(); // -> Gilt für Methoden die "void" sind und keine Parameter haben
         static void Main(string[] args)
         {
+            #region Delegaten
+            ////A();
+            ////B();
+            ////C(12);
 
-            //A();
-            //B();
-            //C(12);
+            ////MeinDelegat del = new MeinDelegat(A);
+            ////del += B;
+            ////del += C;
 
-            //MeinDelegat del = new MeinDelegat(A);
-            //del += B;
-            //del += C;
+            //// typeof(Program).GetMethods()
 
-            // typeof(Program).GetMethods()
+            //// del.Invoke();
 
-            // del.Invoke();
-
-            // Action<T> -> void     (sub)
-            // Func<T>   -> rückgabe (funktionen)
+            //// Action<T> -> void     (sub)
+            //// Func<T>   -> rückgabe (funktionen)
 
 
-            // Action a = new Action(A);
-            Action a = A;
-            Action<int> fürC = C; // Action void mit int als Parameter
+            //// Action a = new Action(A);
+            //Action a = A;
+            //Action<int> fürC = C; // Action void mit int als Parameter
 
-            a.Invoke();
-            fürC.Invoke(12345);
+            //a.Invoke();
+            //fürC.Invoke(12345);
 
-            // Func<T> -> gibt immer einen Wert zurück
-            Func<int, int, int> rechenart = Add;
-            int ergebnis = rechenart.Invoke(12, 5);
-            Console.WriteLine(ergebnis);
+            //// Func<T> -> gibt immer einen Wert zurück
+            //Func<int, int, int> rechenart = Add;
+            //int ergebnis = rechenart.Invoke(12, 5);
+            //Console.WriteLine(ergebnis); 
+            #endregion
+
+            Button b = new Button();
+            b.KlickEvent += MeinButtonKlick;
+            b.KlickEvent += Logger;
+
+            b.Klick();
+
+            // b.KlickEvent = null;            // absolut verboten !!!
+
+            b.Klick();
+            b.Klick();
+
+            b.KlickEvent -= Logger;
+
+            b.Klick();
+            b.Klick();
+
+            // b.KlickEvent.Invoke();      // verboten
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+
+        private static void Logger()
+        {
+            Console.WriteLine($"[{DateTime.Now}] Button wurde geklickt");
+        }
+
+        private static void MeinButtonKlick()
+        {
+            Console.WriteLine("*klick*");
+            Console.Beep();
         }
 
         public static void A()
