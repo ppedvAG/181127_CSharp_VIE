00c8e52d12a0093cb37c00f102beaceebd12164e
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-29 16:40:44 +0100

    System.IO mit Streams und File

--- HalloEditor/MainWindow.xaml.cs
+++ HalloEditor/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,39 +29,60 @@ namespace HalloEditor
 
         private void MenuItemSpeichern_Click(object sender, RoutedEventArgs e)
         {
-            // System.IO 
-            FileStream stream = new FileStream("demo.txt", FileMode.Create);
-            byte[] data = Encoding.Default.GetBytes(textBoxInhalt.Text);
-            stream.Write(data, 0, data.Length);
-            //Flush -> Schreibbuffer
-            stream.Flush();
-            stream.Close();
+            #region FileStream
+            //// System.IO 
+            //FileStream stream = new FileStream("demo.txt", FileMode.Create);
+            //byte[] data = Encoding.Default.GetBytes(textBoxInhalt.Text);
+            //stream.Write(data, 0, data.Length);
+            ////Flush -> Schreibbuffer
+            //stream.Flush();
+            //stream.Close(); 
+            #endregion
+
+            //StreamWriter sw = new StreamWriter("demo.txt");
+            //sw.WriteLine(textBoxInhalt.Text);
+            //sw.Flush();
+            //sw.Close();
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            if (dlg.ShowDialog() == true)
+                File.WriteAllText(dlg.FileName, textBoxInhalt.Text);
         }
 
         private void MenuItemÖffnen_Click(object sender, RoutedEventArgs e)
         {
-            FileStream stream = new FileStream("demo.txt", FileMode.Open);
+            #region FileStream
+            //FileStream stream = new FileStream("demo.txt", FileMode.Open);
 
-            byte[] data = new byte[stream.Length];
-            int index = 0;
-            while (stream.CanRead)
-            {
-                int value = stream.ReadByte();
-                if (value != -1) // 255
-                    data[index] = (byte)value;
-                else
-                    break;
+            //byte[] data = new byte[stream.Length];
+            //int index = 0;
+            //while (stream.CanRead)
+            //{
+            //    int value = stream.ReadByte();
+            //    if (value != -1) // 255
+            //        data[index] = (byte)value;
+            //    else
+            //        break;
 
-                index++;
-            }
-            stream.Close();
+            //    index++;
+            //}
+            //stream.Close(); 
+            #endregion
 
-            textBoxInhalt.Text =  Encoding.Default.GetString(data);
+            //StreamReader sr = new StreamReader("demo.txt");
+            //textBoxInhalt.Text = sr.ReadToEnd();
+            //sr.Close();
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            // ... Eigenschaften
+            if(dlg.ShowDialog() == true)
+                textBoxInhalt.Text = File.ReadAllText(dlg.FileName);
         }
 
         private void MenuItemBeenden_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
+            // Environment.Exit(0);
         }
     }
 }
