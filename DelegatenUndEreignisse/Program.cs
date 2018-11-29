7a1f24e5c7463cd9d98a0e3889c4f60983640a0d
Michael Zöhling <Michael.Zoehling@outlook.com>
2018-11-29 11:39:48 +0100

    Action und Function

---
+++ DelegatenUndEreignisse/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatenUndEreignisse
+{
+    class Program
+    {
+        // Delegat-Typ - Methodensignatur
+        public delegate void MeinDelegat(); // -> Gilt für Methoden die "void" sind und keine Parameter haben
+        static void Main(string[] args)
+        {
+
+            //A();
+            //B();
+            //C(12);
+
+            //MeinDelegat del = new MeinDelegat(A);
+            //del += B;
+            //del += C;
+
+            // typeof(Program).GetMethods()
+
+            // del.Invoke();
+
+            // Action<T> -> void     (sub)
+            // Func<T>   -> rückgabe (funktionen)
+
+
+            // Action a = new Action(A);
+            Action a = A;
+            Action<int> fürC = C; // Action void mit int als Parameter
+
+            a.Invoke();
+            fürC.Invoke(12345);
+
+            // Func<T> -> gibt immer einen Wert zurück
+            Func<int, int, int> rechenart = Add;
+            int ergebnis = rechenart.Invoke(12, 5);
+            Console.WriteLine(ergebnis);
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
+        }
+
+        public static void A()
+        {
+            Console.WriteLine("A");
+        }
+        public static void B()
+        {
+            Console.WriteLine("B");
+        }
+        public static void C(int zahl)
+        {
+            Console.WriteLine("C");
+        }
+
+        public static int Add(int z1,int z2)
+        {
+            return z1 + z2;
+        }
+    }
+}
